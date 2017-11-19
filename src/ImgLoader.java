a9fcb8ce7c2ae540e9c14f61122b2304226eeaa8
DanielArmengod <danielarmengodsorrosal@gmail.com>
2017-11-19 14:07:01 +0100

    Fase de aprendizaje del patron de las imágenes finalizada.

--- src/ImgLoader.java
+++ src/ImgLoader.java
@@ -5,37 +5,36 @@ import java.io.File;
 import java.io.IOException;
 
 public class ImgLoader {
-    public static int[] readImage(BufferedImage source){
+    static int imagenes[][] = new int[9999][9999];
+    static int cont = 0;
+
+    public static void readImage(BufferedImage source){
         final int[] ib = ((DataBufferInt)source.getRaster().getDataBuffer()).getData();
         int img[] = new int[ib.length];
         switch ( source.getType() ){
             case BufferedImage.TYPE_INT_RGB :
-                for (int i=0, b=0 ; i < ib.length ; i++, b+=4){
+                for (int i=0, b=0 ; i < 9999 ; i++, b+=4){
                     int p = ib[i];
-                    ib[i] = p;
+                    imagenes[cont][i] = p;
                     System.out.println(p);
 
                 }
+                cont++;
         }
-        return img;
+
     }
 
-    public static void mainLoadImages(String... args){
-        int imagenes[][] = new int[4000][4000];
+    public static void main(String... args){
+
         try {
             File fil = new File("img");
             File[] files = fil.listFiles();
-            int cont = 0;
             for (File file : files){
                 BufferedImage originImg = ImageIO.read(file);
                 BufferedImage img1 = new BufferedImage(originImg.getWidth(), originImg.getHeight(), BufferedImage.TYPE_INT_RGB);
                 img1.getGraphics().drawImage(originImg, 0, 0, null);
-                int[] listaImagen = ImgLoader.readImage(img1);
-                for(int numero : listaImagen){
-
-                }
-                //imagenes[cont][]
-                cont++;
+                ImgLoader.readImage(img1);
+                MotorAI.learn(imagenes);
             }
         } catch (IOException e) {
             e.printStackTrace();
--- src/MotorAI.java
+++ src/MotorAI.java
@@ -1,75 +1,72 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class MotorAI {
+    static int[]matrix;
     static final double E = 0.6;
-    public static void main(String ...args){
+    public static int[] learn(int[][] tb){
         //entrada
-        int[][] tb = {{1, 1, 1},{1, -1, 1}, {-1, -1, -1}, {-1, 1, 1}};
+        //int[][] tb = {{1, 1, 1},{1, -1, 1}, {-1, -1, -1}, {-1, 1, 1}};
+        double[][] xRes = new double[9999][9999];
 
-        double w1, w2, θ, y;
-        w1 = new Random().nextDouble() / 2.5;
-        w2 = new Random().nextDouble() / 2.5;
+        double w, θ, y;
+
+        for (int i = 0; i < tb.length; i++) {
+            for (int j = 0; j < tb[i].length; j++) {
+                w = new Random().nextDouble() / 2.5;
+                xRes[i][j] = w;
+            }
+        }
         θ = -0.4;
 
         y = 0;
-        System.out.println("w1: " + w1);
-        System.out.println("w2: " + w2);
-        System.out.println("θ: " + θ);
 
-        //INICIACION DEL APRENDIZAJE MEDIANTE LA PUERTA LÓGICA OR
+        System.out.println("θ: " + θ);
 
         int i, cont;
         i = 0;
         cont = 1;
 
         while(i < tb.length && cont < 100){
-            y = Math.tanh((tb[i][0] * w1) + (tb[i][1] * w2) + (-1 * 0));
-            y = (y >= 0) ? 1 : -1;
+            for (int j = 0; j < tb[i].length; j++) {
+                y = Math.tanh((tb[i][j] * xRes[i][j]) + (tb[i][j] * xRes[i][j]) + (-1 * 0));
+                y = (y >= 0) ? 1 : -1;
 
-            System.out.println("Entrada[" + tb[i][0] + "," + tb[i][1]
-                    + "]) Valor esperado[" + tb[i][2]
-                    + "] Salida[" + (int) y + "]");
-            if (y == tb[i][2]) {
-                i++;
-            } else {
-                System.out.println("Valor esperado diferente de la salida. Hay que reajustar pesos...");
-                //Ajuste de pesos
-                w1 = w1 + 2 * E * tb[i][2] * tb[i][0];
-                w2 = w2 + 2 * E * tb[i][2] * tb[i][1];
-                θ = θ + 2 * E * tb[i][2] * (-1);
+                System.out.println("Entrada[" + tb[i][j] + "," + tb[i][j]
+                        + "]) Valor esperado[" + tb[i][j]
+                        + "] Salida[" + (int) y + "]");
+                if (y == tb[i][j]) {
+                    i++;
+                } else {
+                    System.out.println("Valor esperado diferente de la salida. Hay que reajustar pesos...");
+                    //Ajuste de pesos
+                    xRes[i][j] = xRes[i][j] + 2 * E * tb[i][j] * tb[i][j];
+                    θ = θ + 2 * E * tb[i][j] * (-1);
 
-                System.out.println("\nAjuste de pesos (" + cont + "):");
-                System.out.println("w1: " + w1);
-                System.out.println("w2: " + w2);
-                System.out.println("θ: " + θ + "\n");
-                cont++;
-                i = 0;
+                    System.out.println("\nAjuste de pesos (" + cont + "):");
+                    System.out.println("w1: " + xRes[i][j]);
+                    System.out.println("θ: " + θ + "\n");
+                    cont++;
+                    i = 0;
+                }
             }
         }
 
         if (cont <= 99999) {
-            System.out.println("\nFase de aprendizaje terminado con exito ");
-            System.out.println("\nResultados:");
-            System.out.println("w1: " + w1);
-            System.out.println("w2: " + w2);
-            System.out.println("θ: " + θ);
-            System.out.println("\nIniciando fase de testeo...");
-            System.out.println("Introduce Entrada 1 (X1): ");
-            Scanner leerX1 = new Scanner(System.in);
-            double x1 = Double.parseDouble(leerX1.next());
-
-            System.out.println("Introduce Entrada 2 (X2): ");
-            Scanner leerX2 = new Scanner(System.in);
-            double x2 = Double.parseDouble(leerX2.next());
-
-            y = Math.tanh((x1 * w1) + (x2 * w2) + (-1 * θ));
-            y = (y >= θ) ? 1 : -1;
+            //x1, x2 lo que se introduce por teclado
+            matrix = new int[tb.length];
+            //lo generado
+            for (int j = 0; j < xRes.length; j++) {
+                for (int k = 0; k < xRes[i].length; k++) {
+                    matrix[j] =(int) Math.tanh((tb[0][i] * xRes[j][k]) + (-1 * θ));
+                }
+            }
 
             System.out.println("\nSalida: " + (int)y);
         } else {
             System.out.println("\nFase de aprendizaje ha fallado\n");
         }
+
+        return matrix;
         
     }
     
