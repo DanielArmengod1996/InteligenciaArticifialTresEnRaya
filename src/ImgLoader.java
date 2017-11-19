7830f79dccd8216b8816f17639af5d7b220b4207
DanielArmengod1996 <danielarmengodsorrosal@gmail.com>
2017-11-19 13:00:40 +0100

    IMG load images by the route of a folder

--- src/ImgLoader.java
+++ src/ImgLoader.java
@@ -5,24 +5,38 @@ import java.io.File;
 import java.io.IOException;
 
 public class ImgLoader {
-    public static void readImage(BufferedImage source){
+    public static int[] readImage(BufferedImage source){
         final int[] ib = ((DataBufferInt)source.getRaster().getDataBuffer()).getData();
-
+        int img[] = new int[ib.length];
         switch ( source.getType() ){
             case BufferedImage.TYPE_INT_RGB :
                 for (int i=0, b=0 ; i < ib.length ; i++, b+=4){
                     int p = ib[i];
+                    ib[i] = p;
                     System.out.println(p);
+
                 }
         }
+        return img;
     }
 
-    public static void main(String... args){
+    public static void mainLoadImages(String... args){
+        int imagenes[][] = new int[4000][4000];
         try {
-            BufferedImage im = ImageIO.read(new File("img/testImagen.jpg"));
-            BufferedImage img = new BufferedImage(im.getWidth(), im.getHeight(), BufferedImage.TYPE_INT_RGB);
-            img.getGraphics().drawImage(im, 0, 0, null);
-            ImgLoader.readImage(img);
+            File fil = new File("img");
+            File[] files = fil.listFiles();
+            int cont = 0;
+            for (File file : files){
+                BufferedImage originImg = ImageIO.read(file);
+                BufferedImage img1 = new BufferedImage(originImg.getWidth(), originImg.getHeight(), BufferedImage.TYPE_INT_RGB);
+                img1.getGraphics().drawImage(originImg, 0, 0, null);
+                int[] listaImagen = ImgLoader.readImage(img1);
+                for(int numero : listaImagen){
+
+                }
+                //imagenes[cont][]
+                cont++;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
